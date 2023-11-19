97b6b4da162caca17bb195d24a352559b579c051
pavel <pavel_vilkov17@mail.ru>
2023-11-19 14:45:12 +0700

    варианты создания массивов в golang

--- AverefeInArraray.go
+++ AverefeInArraray.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var numbers [5]int = [5]int{1, 2, 3, 4, 5}
+	var num = [...]int{1, 2, 4, 6, 9, 23}
+	fmt.Println(numbers)
+	fmt.Println(num)
 }
